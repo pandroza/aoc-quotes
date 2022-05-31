732aa641bcd73d3feaa38e9e0e97bb04d4ac654d
pandroza <60747184+pandroza@users.noreply.github.com>
2022-05-31 04:57:40 +0100

    aoc5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'jquery';
 import 'css-doodle';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
