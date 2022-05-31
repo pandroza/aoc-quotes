8015f2a0d45cfba2e2f4b13470948b9b57aec762
pandroza <60747184+pandroza@users.noreply.github.com>
2022-05-31 04:49:11 +0100

    aoc4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
-import App from './App';
+import './index.css';
+import 'css-doodle';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+function App() {
+  return (
+    <css-doodle>{`
+      :doodle {
+        @grid: 2 / 200px;
+        grid: 1px;
+      }
+      background: @pick(red, pink);
+    `}</css-doodle>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,4 +25,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+reportWebVitals();
